99dd8741e0314465d87e3379390a272d77963057
UltimateQuack <patoflamejantetv@gmail.com>
2025-04-12 11:36:44 -0300

    fix?: added ReadLine() to only run on debug builds

--- qfmain/mainClass.vb
+++ qfmain/mainClass.vb
@@ -1,8 +1,14 @@
 Imports quackfetchcore
 
-Module Program
-    Sub Main()
-        Console.WriteLine("Machine ID: " & quackfetchcore.MachineInfo.GetMachineId())
-        'Console.ReadLine()
+Module Program '"Program" means that this module is a VB.NET program
+               'and bla bla bla
+    Sub Main() ' Main function, the one gets called when the app starts
+        Console.WriteLine(
+            "Machine ID: " & 
+            quackfetchcore.MachineInfo.GetMachineId()
+                          )
+        #If DEBUG Then
+            Console.ReadLine()
+        #End If
     End Sub
 End Module
